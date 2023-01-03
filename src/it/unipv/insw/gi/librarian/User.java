c0eec29289b9bb638913c7cab16f89e344ccc7ef
kamde <kamde@LAPTOP-KPVF8KME>
2023-01-03 17:10:06 +0100

    first commit del progetto

--- src/it/unipv/insw/gi/librarian/User.java
+++ src/it/unipv/insw/gi/librarian/User.java
@@ -1,5 +1,24 @@
 package it.unipv.insw.gi.librarian;
 
 public class User {
+	private String nome;
+	private String cognome;
+	private String indirizzo;
+	public User(String unNome, String unCognome,String unIndirizzo) {
+		nome = unNome;
+		cognome = unCognome;
+		indirizzo = unIndirizzo;
+	}
+		public String getCognome() {
+			return cognome;
+		}
+		
+		public String getNome() {
+			return nome;
+		}
+		public String getIndirizzo() {
+			return indirizzo;
+		}
+	}
 
-}
+
