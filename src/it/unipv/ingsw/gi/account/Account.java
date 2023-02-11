149eef722e4e2ca26dfa17191bf22106f110b7f5
Abdelrahman Nassar <abdelrahman.nassar01@universitadipavia.it>
2023-02-11 11:45:08 +0100

    small change in the class account

--- src/it/unipv/ingsw/gi/account/Account.java
+++ src/it/unipv/ingsw/gi/account/Account.java
@@ -1,5 +1,57 @@
 package it.unipv.ingsw.gi.account;
 
+import java.util.*;
+
 public class Account {
+	public double id;
+	public double OPENDATE;
+	ArrayList History = new ArrayList();
+	public AccountState state;
+	LinkedList dueBooks = new LinkedList();
+	
+	
+	public Account(double id, double oPENDATE, ArrayList history, AccountState state, LinkedList dueBooks) {
+		super();
+		this.id = id;
+		OPENDATE = oPENDATE;
+		History = history;
+		this.state = state;
+		this.dueBooks = dueBooks;
+	}
+
+
+	private double getId() {
+		return id;
+	}
+
+
+	private void setId(double id) {
+		this.id = id;
+	}
+
+
+	private double getOPENDATE() {
+		return OPENDATE;
+	}
+
+
+	private void setOPENDATE(double oPENDATE) {
+		OPENDATE = oPENDATE;
+	}
+
+
+	private AccountState getState() {
+		return state;
+	}
+
+
+	private void setState(AccountState state) {
+		this.state = state;
+	}
+	
+	
+	
+	
+	
 
 }
